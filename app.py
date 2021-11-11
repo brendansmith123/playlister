8145da15f51113bf7d2f725db192132c990137f9
Brendan Smith <brendansmith@Brendans-MacBook-Air.local>
2021-11-10 20:28:55 -0600

    Users can create playlists

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -7,13 +7,26 @@ playlists = db.playlists
 
 app = Flask(__name__)
 
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        # We know that embedded YouTube videos always have this format
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
+
+# app.py
+
+# playlists = [
+#   { 'title': 'Great Playlist' },
+#   { 'title': 'Next Playlist' }
+# ]
+
 @app.route('/')
-# OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
->
+def playlists_index():
+    """Show all playlists."""
+    # Update this line
+    return render_template('playlists_index.html', playlists=playlists.find())
 @app.route('/playlists')
 def playlists_index():
     """Show all playlists."""
@@ -25,4 +38,28 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+# Note the methods parameter that explicitly tells the route that this is a POST
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    # Grab the video IDs and make a list out of them
+    video_ids = request.form.get('video_ids').split()
+    # call our helper function to create the list of links
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
+
 
